5ea1ca3490050b6b461c82657c817d1d12b39e06
Jasper <jasper.lammers@gmail.com>
2025-12-11 16:29:36 +0100

    Implemented CodeCasa.Lights.

---
+++ src/CodeCasa.Lights/Extensions/LightExtensions.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CodeCasa.Lights.Extensions;
+
+public static class LightExtensions
+{
+    public static ILight[] Flatten(this ILight light)
+    {
+        var visitedLights = new HashSet<ILight>();
+        var result = new List<ILight>();
+        FlattenRecursive(light, visitedLights, result);
+        return result.ToArray();
+    }
+
+    private static void FlattenRecursive(ILight light, HashSet<ILight> visitedLights, List<ILight> result)
+    {
+        if (!visitedLights.Add(light))
+        {
+            return;
+        }
+
+        var children = light.GetChildren();
+        if (!children.Any())
+        {
+            result.Add(light);
+            return;
+        }
+        foreach (var child in children)
+        {
+            FlattenRecursive(child, visitedLights, result);
+        }
+    }
+}
---
+++ src/CodeCasa.Lights/ILight.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeCasa.Lights;
+
+/// <summary>
+/// Represents a single light or group of lights.
+/// </summary>
+public interface ILight
+{
+    LightParameters Parameters { get; }
+    void ApplyTransition(LightTransition transition);
+    ILight[] GetChildren();
+}
